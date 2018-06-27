8189ad78c8748c817787d5eea1df0161bc8ede68
Guofeng Xi <guofeng.xi@nio.com>
2018-06-27 15:48:21 +0800

    Express: multi-images upload

--- src/express/file_upload/server.js
+++ src/express/file_upload/server.js
@@ -9,11 +9,39 @@ const app = express()
 app.use(cors())
 
 const UPLOAD_PATH = 'uploads'
-const upload = multer({ dest: `${UPLOAD_PATH}`, fileFilter: imageFilter})
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, `${UPLOAD_PATH}`)
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname + '-' + Date.now())
+  }
+})
+const upload = multer({ storage: storage, fileFilter: imageFilter})
 
-app.post('/upload', upload.single('avatar'), (req, res) => {
-  if (req.file) {
-    res.send('File Upload!')
+const parseFile = function(file) {
+  return {
+    filename: file.filename,
+    originalname: file.originalname,
+    encoding: file.encoding,
+    path: file.path,
+    mimetype: file.mimetype
+  }
+}
+
+app.post('/image/multi-upload', upload.array('images', 12), (req, res) => {
+  let files = req.files
+  if (files.length > 0) {
+    res.send(files.map(file => {
+      return parseFile(file)
+    }))
+  }
+})
+
+app.post('/image/upload', upload.single('images'), (req, res) => {
+  let file = req.file
+  if (file) {
+    res.send(parseFile(file))
   } else {
     throw Error("No file attached")
   }
